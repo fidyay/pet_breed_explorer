87452e2dcaf9c8b38e794d28b2ed1bbea0e49531
Fidyay <fidiy1878@gmail.com>
2025-08-22 21:52:52 +0300

    debugged page a bit and added card components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import {
   getRandomDogsBreeds,
   getRandomCatsBreeds,
 } from "@/lib/functions/server";
+import { connection } from "next/server";
 
 export default async function Home() {
+  // I use it here to disable prerendering of this page as we expect different results every time
+  await connection();
+
   const [catsBreeds, dogsBreeds] = await Promise.all([
     getRandomCatsBreeds(),
     getRandomDogsBreeds(),
